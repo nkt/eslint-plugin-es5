f88ace5f02c0afb1a63142729d058ee1ec618d6f
Nikita Gusakov <gusnkt@gmail.com>
2017-01-04 00:59:18 +0300

    feat: add no-unicode-regex rule

--- src/rules/no-unicode-regex.js
+++ src/rules/no-unicode-regex.js
@@ -8,8 +8,21 @@ module.exports = {
     schema: []
   },
   create(context) {
+    function report(node) {
+      context.report({
+        node,
+        message: 'Unexpected Unicode RegExp.'
+      });
+    }
+
     return {
-      
+      Literal(node) {
+        if (node.regex) {
+          if (node.regex.flags.includes('u')) {
+            report(node);
+          }
+        }
+      }
     };
   }
 };
--- tests/rules/no-unicode-regex.js
+++ tests/rules/no-unicode-regex.js
@@ -2,9 +2,14 @@
 
 module.exports = {
   valid: [
-
+    '/foo/;',
+    '/foo/g;',
+    'var foo = /foo/;'
   ],
   invalid: [
-
+    { code: '/foo/u', errors: [{ message: 'Unexpected Unicode RegExp.' }] },
+    { code: '/foo/ug', errors: [{ message: 'Unexpected Unicode RegExp.' }] },
+    { code: '/foo/gu', errors: [{ message: 'Unexpected Unicode RegExp.' }] },
+    { code: 'var foo = /foo/u;', errors: [{ message: 'Unexpected Unicode RegExp.' }] },
   ]
 };
