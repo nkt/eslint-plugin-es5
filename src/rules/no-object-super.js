288c681f79604e7ddc5380d103d345a800cca84c
Nikita Gusakov <gusnkt@gmail.com>
2017-01-04 00:52:54 +0300

    feat: add no-object-super rule

--- src/rules/no-object-super.js
+++ src/rules/no-object-super.js
@@ -3,13 +3,36 @@
 module.exports = {
   meta: {
     docs: {
-      description: ''
+      description: 'deny call super()'
     },
     schema: []
   },
   create(context) {
+    function isSuperCall(node) {
+      if (node.callee) {
+        if (node.callee.type === 'Super') {
+          return true;
+        }
+
+        if (node.callee.object && node.callee.object.type) {
+          if (node.callee.object.type === 'Super') {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
+
     return {
-      
+      CallExpression(node) {
+        if (isSuperCall(node)) {
+          context.report({
+            node,
+            message: 'Unexpected super() call.'
+          });
+        }
+      }
     };
   }
 };
--- tests/rules/no-object-super.js
+++ tests/rules/no-object-super.js
@@ -2,9 +2,10 @@
 
 module.exports = {
   valid: [
-
+    'function foo() {}'
   ],
   invalid: [
-
+    { code: 'function foo() { super(); }', errors: [{ message: 'Unexpected super() call.' }] },
+    { code: 'function foo() { super.foo(); }', errors: [{ message: 'Unexpected super() call.' }] },
   ]
 };
