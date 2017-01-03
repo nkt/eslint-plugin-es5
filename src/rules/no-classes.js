43ec43ca50ef301af428a0ce69f3ba6e9c9aeb01
Nikita Gusakov <gusnkt@gmail.com>
2017-01-03 18:54:10 +0300

    feat: add no-classes rule

--- src/rules/no-classes.js
+++ src/rules/no-classes.js
@@ -3,13 +3,25 @@
 module.exports = {
   meta: {
     docs: {
-      description: ''
+      description: 'deny classes'
     },
     schema: []
   },
   create(context) {
+    function report(node) {
+      context.report({
+        node,
+        message: 'Unexpected class declaration. Use function instead.'
+      });
+    }
+
     return {
-      
+      ClassDeclaration(node) {
+        report(node);
+      },
+      ClassExpression(node) {
+        report(node);
+      }
     };
   }
 };
--- tests/rules/no-classes.js
+++ tests/rules/no-classes.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   valid: [
-
+    'function foo() {}',
+    'var foo = function () {};'
   ],
   invalid: [
-
+    { code: 'class foo {}', errors: [{ message: 'Unexpected class declaration. Use function instead.' }] },
+    { code: 'var foo = class {}', errors: [{ message: 'Unexpected class declaration. Use function instead.' }] }
   ]
 };
