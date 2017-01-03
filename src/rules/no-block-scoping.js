8ea7b4ec4a5e1efe9ba71fac92e58c17ba44ae5a
Nikita Gusakov <gusnkt@gmail.com>
2017-01-03 18:04:56 +0300

    feat: add no-block-scoping rule

--- src/rules/no-block-scoping.js
+++ src/rules/no-block-scoping.js
@@ -3,13 +3,30 @@
 module.exports = {
   meta: {
     docs: {
-      description: ''
+      description: 'deny block-scoping'
     },
-    schema: []
+    schema: [{
+      type: 'object',
+      properties: {
+        let: { type: 'boolean' },
+        const: { type: 'boolean' }
+      }
+    }]
   },
   create(context) {
     return {
-      
+      VariableDeclaration(node) {
+        const options = Object.assign({ let: false, const: false }, context.options[0]);
+
+        if (node.kind === 'var' || options[node.kind]) {
+          return;
+        }
+
+        context.report({
+          node,
+          message: `Unexpected ${node.kind} declaration.`
+        });
+      }
     };
   }
 };
--- tests/rules/no-block-scoping.js
+++ tests/rules/no-block-scoping.js
@@ -2,9 +2,12 @@
 
 module.exports = {
   valid: [
-
+    'var foo = 1;',
+    { code: 'let foo = 1;', options: [{ let: true }] },
+    { code: 'const foo = 1;', options: [{ const: true }] }
   ],
   invalid: [
-
+    { code: 'let foo = 1;', errors: [{ message: 'Unexpected let declaration.' }] },
+    { code: 'const foo = 1;', errors: [{ message: 'Unexpected const declaration.' }] }
   ]
 };
