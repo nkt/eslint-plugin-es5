5837d91a57e4d223f0dae775769da3d7fb904d8e
ota <otameshiyo23@gmail.com>
2018-03-31 01:18:57 +0900

    [update] Make `es5/no-spread` fixable

--- tests/rules/no-spread.js
+++ tests/rules/no-spread.js
@@ -8,9 +8,40 @@ module.exports = {
     'function foo (...args) {}'
   ],
   invalid: [
-    { code: 'foo(...args)', errors: [{ message: 'Unexpected spread expression.' }] },
-    { code: 'foo(a, b, c, ...args)', errors: [{ message: 'Unexpected spread expression.' }] },
-    { code: '[...args]', errors: [{ message: 'Unexpected spread expression.' }] },
-    { code: '[1, ...[2], 3]', errors: [{ message: 'Unexpected spread expression.' }] }
+    {
+      code: 'foo(...args)',
+      output: null,
+      errors: [{ message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: 'foo(a, b, c, ...args)',
+      output: null,
+      errors: [{ message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: '[...args]',
+      output: '[].concat(args)',
+      errors: [{ message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: '[1, ...[2], 3]',
+      output: '[].concat([1],[2],[3])',
+      errors: [{ message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: '[1, ...args, ]',
+      output: '[].concat([1],args)',
+      errors: [{ message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: '["a", ...b(), ...c]',
+      output: '[].concat(["a"],b(),c)',
+      errors: [{ message: 'Unexpected spread expression.' }, { message: 'Unexpected spread expression.' }]
+    },
+    {
+      code: '[...[...args]]',
+      output: '[].concat([...args])',
+      errors: [{ message: 'Unexpected spread expression.' }, { message: 'Unexpected spread expression.' }]
+    }
   ]
 };
