470d588607ff0c9f98a9b570d182cdb708d134cf
Nikita Chuklinov <nchuklinov@ambrahealth.com>
2020-06-27 01:32:41 +0300

    Added exceptMethods and additionalMethods options to no-es6-methods

--- tests/rules/no-es6-methods.js
+++ tests/rules/no-es6-methods.js
@@ -8,10 +8,19 @@ module.exports = {
     '_.chain([1, 2, 3]).reverse().find().value()',
     '$("#myDiv").find(".child")',
     'jQuery("#myDiv").parent().find(".child")',
-    '$myForm.find(".child")'
+    '$myForm.find(".child")',
+    '[[1, 2], [3, 4]].flat()',
+    { code: 'jqVariable.find(x => x == 3);', options: [{ exceptMethods: ['find'] }] },
+    { code: 'a.includes("123");', options: [{ exceptMethods: ['includes'] }] },
   ],
   invalid: [
     { code: '[1, 2, 3].find(x => x == 3);', errors: [{ message: 'ES6 methods not allowed: find' }] },
-    { code: '[0, 0, 0].fill(7, 1);', errors: [{ message: 'ES6 methods not allowed: fill' }] }
+    { code: '[0, 0, 0].fill(7, 1);', errors: [{ message: 'ES6 methods not allowed: fill' }] },
+    { code: '[1, 2, 3].filter(x => x !== 3);',
+      options: [{ exceptMethods: ['map'], additionalMethods: ['filter'] }],
+      errors: [{ message: 'ES6 methods not allowed: filter' }] },
+    { code: '[[1, 2], [3, 4]].flat()',
+      options: [{ exceptMethods: ['map'], additionalMethods: ['flat'] }],
+      errors: [{ message: 'ES6 methods not allowed: flat' }] }
   ]
 };
